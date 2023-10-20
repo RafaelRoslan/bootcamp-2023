f2f6162d7f49afac63565e3aff4ce602a8e0a671
Rafael Roslan <rafaelroslan@gmail.com>
2023-10-20 12:38:07 -0300

    first commit

---
+++ src/test/java/edu/projeto/bootcamp2023/Bootcamp2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.projeto.bootcamp2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
